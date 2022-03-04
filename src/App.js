69f3658feb2fdbd52b903232f30add34ecdd863d
majd maihoub <majd.maihoub@meta-itech.com>
2022-03-05 00:05:45 +0200

    fixig styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import Home from './Pages/home/home.js';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Home />
     </div>
   );
 }
 
-export default App;
+//set the steps initl values
+//set the width of the bar
+//set the value of the indicator
+
+//set filled amount function
+//define the highest limit that the value can reach (prevent overflow)
+//define the amount completed value 4000/60-0
+
+//indicator
