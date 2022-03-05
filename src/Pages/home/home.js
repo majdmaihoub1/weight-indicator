ff5283e94e4223921cd73d3d3ba26080ffc12c0f
majd maihoub <majd.maihoub@meta-itech.com>
2022-03-05 14:03:49 +0200

    fixing negative value with state

--- src/Pages/home/home.js
+++ src/Pages/home/home.js
@@ -4,14 +4,14 @@ import TextField from '@mui/material/TextField';
 import './home.style.css';
 
 const Home = () => {
-  const [weight, setWeight] = useState(0);
+  const [weight, setWeight] = useState();
 
   const handleChange = (e) => {
-    e.target.value >= 120
-      ? setWeight(120)
-      : weight <= 0
-      ? setWeight(0)
-      : setWeight(e.target.value);
+    let val = parseInt(e.target.value, 10);
+    if (isNaN(val)) {
+      setWeight('');
+    } else val = val >= 0 ? val : val > 120 ? 120 : 0;
+    setWeight(val);
   };
 
   const weightValue =
@@ -23,6 +23,7 @@ const Home = () => {
       <div className='center'>
         <div className='weight-input'>
           <TextField
+            value={weight}
             placeholder='Enter your pet`s weight'
             inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
             type='number'
@@ -39,8 +40,8 @@ const Home = () => {
           />
         </div>
         <div className='weight-info'>
-          <h3> Mr Nash's is {weightValue}</h3>
-          <h2>Mr Nash's PMI is {weight}</h2>
+          <h3> Mr Nash is {weightValue}</h3>
+          <h2>Mr Nash's PMI is {weight ? weight : 'Not A valid Value'}</h2>
         </div>
         <div className='indicator'>
           <WeightIndicator
